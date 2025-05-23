b37758b5fa1656c0ef759dc6c866477dd584dc42
Guangning Zhang <gzha0824@uni.sydney.edu.au>
2025-05-24 04:42:38 +1000

    feat: Completed the command receive from threads. Added a timeout to select, for each thread to make them able to check the interupt after each timeout instead of blocking. The client can now send DISCONNECT command to server.

--- src/client.c
+++ src/client.c
@@ -1,4 +1,5 @@
 #define _POSIX_C_SOURCE 200809L
+#define BUFF_LEN 1024
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,24 +8,33 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdbool.h>
+#include <string.h>
 
-
-bool conn_success = false;
+volatile int interupted = 0;
 
 void handle_SIGRTMIN(int sig){
-    if(sig == SIGRTMIN+1){
-        printf("Received SIGRTMIN+1.\n");
-        conn_success = true;
-    }else{
-        printf("Runtime signal is not correctly responsed.\n");
-        conn_success = false;
-    }
+    printf("Server responses. Establish connection.\n");
+}
+
+void handle_SIGINT(int sig){
+    printf("Got interrupted.\n");
+    interupted = 1;
+}
+
+// On receiving SIGUSR1. Means the server is down.
+// Terminate the client.
+void handle_SIGUSR1(int sig){
+    interupted = 1;
 }
 
 
 int main(int argc, char *argv[]){
     // Check input arguments.
     long server_pid_value;
+    char username[BUFF_LEN];
+    char permission[BUFF_LEN];
+    char buff[BUFF_LEN];
+    bool authorised = false;
     if(argc != 3){
         printf("Please provide the server PID and your username.\n");
         return 1;
@@ -35,9 +45,26 @@ int main(int argc, char *argv[]){
             printf("Please provide positive integer to indicate server PID.");
             return 1;
         }
+        
+        if(strcmp(argv[2], "\n") == 0){
+            printf("Please provide your username.\n");
+            return 1;
+        }else{
+            strcpy(username, argv[2]);
+            // username[strlen(argv[2])] = '\0';
+            printf("Got username: %s\n", username);
+        }
     }
+
+    // Handle SIGUSER1. The server told client to close.
+    signal(SIGUSR1, handle_SIGUSR1);
+    // Handle SIGINT.
+    signal(SIGINT, handle_SIGINT);
     
-    signal(SIGRTMIN+1, handle_SIGRTMIN);
+    struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_handler = handle_SIGRTMIN;
+    sigaction(SIGRTMIN+1, &sa, NULL);
 
     // Send SIGRTMIN to server.
     // Sleep for 1 second for response.
@@ -46,18 +73,66 @@ int main(int argc, char *argv[]){
     // Send its pid to the server as a signal value.
     union sigval value;
     value.sival_int = pid;
+    
+    sigset_t signal_set;
+    int sig;
+    sigemptyset(&signal_set);
+    sigaddset(&signal_set, SIGRTMIN+1);
 
+    // Send the SIGRTMIN with client pid.
     sigqueue(server_pid, SIGRTMIN, value);
-    sigqueue(server_pid, SIGRTMIN, value);
-    sigqueue(server_pid, SIGRTMIN, value);
-    sleep(1);
-    if(!conn_success){
-        printf("Server is not responding.\n");
-        return 1;
-    }
+    sigwait(&signal_set, &sig);
 
     // Connection established.
     printf("Successfully connected to the server.\n");
+
+    // Open the pipes to communicate.
+    char fifo_name1[BUFF_LEN];
+    char fifo_name2[BUFF_LEN];
+    sprintf(fifo_name1, "FIFO_S2C_%d", pid);
+    int read_fd = open(fifo_name1, O_RDONLY);
+    sprintf(fifo_name2, "FIFO_C2S_%d", pid);
+    int write_fd = open(fifo_name2, O_WRONLY);
+
+    // Write the username into the pipe
+    write(write_fd, username, strlen(username));
+
+    // Read the permision return.
+    read(read_fd, buff, BUFF_LEN);
+    printf("Got response: %s.\n", buff);
+    if(strcmp(buff, "Reject UNAUTHORISED") == 0){
+        printf("Your identify is not authorised.\n");
+        authorised = false;
+    }else{
+        strcpy(permission, buff);
+        authorised = true;
+    }
+
+
+    // When the client is not interupted.
+    while(!interupted && authorised){
+        scanf("%s", buff);
+        printf("Got a command: %s.\n", buff);
+        if(strcmp(buff, "DISCONNECT") == 0){
+            // Write the disconnect into pipe.
+            printf("Writing to pipe.\n");
+            write(write_fd, buff, strlen(buff));
+            break;
+        }else{
+            printf("Invalid command.\n");
+        }
+    }
+
+    
+    // Tell the server.
+    printf("Writing to pipe.\n");
+    write(write_fd, "DISCONNECT", strlen("DISCONNECT"));
+
+
+    close(read_fd);
+    close(write_fd);
+    unlink(fifo_name1);
+    unlink(fifo_name2);
     return 0;
 
 }
--- src/server.c
+++ src/server.c
@@ -1,29 +1,160 @@
 #define _POSIX_C_SOURCE 200809L
 #define MAX_CLIENTS 10
+#define BUFF_LEN 1024
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/select.h>
 #include <signal.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <string.h>
+#include <stdbool.h>
 
 
+typedef struct{
+    char* username;
+    char* permission_level;
+}User;
+
+
+// Global variables. Shared in all threads.
 volatile int interupted = 0;
 pthread_t threads[MAX_CLIENTS];
+User** users;
+int total_user = 0;
 int clinet_count = 0;
 
+
+// The thread to handle client request.
 void* server_thread(void* arg){
+    // Resolve the client pid. As a local variable.
+    char username[BUFF_LEN];
+    char permission[BUFF_LEN];
+    char command[BUFF_LEN];
+    bool connecting = true;
     pid_t client_pid = *(pid_t*)arg;
     printf("Created a new thread for client: %d.\n", client_pid);
 
+    // Create fifos to communicate with the client.
+    char fifo_name1[BUFF_LEN];
+    char fifo_name2[BUFF_LEN];
+    sprintf(fifo_name1, "FIFO_S2C_%d", client_pid);
+    printf("S2C FIFO: %s.\n", fifo_name1);
+    if(access(fifo_name1, F_OK) == 0){
+        unlink(fifo_name1);
+    }
+    if(mkfifo(fifo_name1, 0666) < 0){
+        perror("Failed to create pipe.\n");
+    }
+
+    sprintf(fifo_name2, "FIFO_C2S_%d", client_pid);
+    printf("C2S FIFO: %s.\n", fifo_name2);
+    if(access(fifo_name2, F_OK) == 0){
+        unlink(fifo_name2);
+    }
+    if(mkfifo(fifo_name2, 0666) < 0){
+        perror("Failed to create pipe.\n");
+    }
+
     // Send the response to the client.
     kill(client_pid, SIGRTMIN+1);
+    printf("Connection establish response sent to the client: %d.\n", client_pid);
 
-    // Free the client pid before terminate the thread.
+    // Open the pipes to communicate with the client.
+    int write_fd = open(fifo_name1, O_WRONLY);
+    int read_fd = open(fifo_name2, O_RDONLY);
+    printf("Thread listening on %d.\n", read_fd);
+    printf("Thread writing on %d.\n", write_fd);
+
+    // Read the request from the fifo for the client.
+    read(read_fd, username, BUFF_LEN);
+    printf("Got username from pipe: %s.\n", username);
+
+    // Verify user.
+    bool user_exist = false;
+    for(int i = 0; i < total_user; i++){
+        // If find a match.
+        if(strcmp(users[i]->username, username) == 0){
+            // Get the permission level of the matched user.
+            user_exist = true;
+            strcpy(permission, users[i]->permission_level);
+            permission[strlen(permission)] = '\n';
+            permission[strlen(permission)] = '\0';
+            write(write_fd, permission, strlen(permission));
+            printf("Permission written into pipe: %s.\n", permission);
+            break;
+        }
+    }
+    // If the username does not exist.
+    if(!user_exist){
+        printf("The user: %s does not exists in the permission list.\n", username);
+        // Write the rejection into pipe.
+        write(write_fd, "Reject UNAUTHORISED", strlen("Reject UNAUTHORISED"));
+        connecting = false;
+        sleep(1);
+    }
+
+    // Using select to monitor the read_fd.
+    fd_set readfds;
+    struct timeval timeout;
+    while(connecting){
+        // Set timeout for checking interupted. Prevent blocking.
+        timeout.tv_usec = 500000;
+        timeout.tv_usec = 0;
+        FD_ZERO(&readfds);
+        FD_SET(read_fd, &readfds);
+
+        int ret = select(read_fd + 1, &readfds, NULL, NULL, &timeout);
+
+        if(ret < 0){
+            perror("Select failed.\n");
+            break;
+        }else if(ret == 0){
+            // Timeouot.
+            if(interupted){
+                printf("Thread %d detected interruption\n", client_pid);
+                break;
+            }
+            continue;
+        }else if(FD_ISSET(read_fd, &readfds)){
+            // There is data in the read_fd.
+            // Read and handle the client requests from the pipe.
+            read(read_fd, command, BUFF_LEN);
+            printf("Got command: %s from client: %d.\n", command, client_pid);
+
+            // If the command is disconnect.
+            if(strcmp(command, "DISCONNECT") == 0){
+                break;
+            }
+        }
+        
+    }
+
+
+    // On connection lost.
+    // If it is caused by server interuption.
+    if(interupted){
+        // Send an interupt signal to the serving client.
+        printf("Sending SIGUSR1 to client: %d.\n", client_pid);
+        kill(client_pid, SIGUSR1);
+    }
+
+    // Free the resources before terminate the thread.
+    // Client pid.
     free(arg);
-    return NULL;
+    // File descriptors.
+    close(write_fd);
+    close(read_fd);
+    // FIFOs.
+    unlink(fifo_name1);
+    unlink(fifo_name2);
+    printf("Thread %p for client: %d destroyed.\n", (void*)pthread_self(), client_pid);
+
+    clinet_count--;
+    pthread_exit(NULL);
 }
 
 
@@ -49,12 +180,31 @@ void handle_SIGRTMIN(int sig, siginfo_t* info, void* context){
         free(client_pid_ptr);
     }else{
         clinet_count++;
-        printf("Success created a thread.\n");
+        printf("Successfully created a thread.\n");
     }
 }
 
+// On interupt or QUIT calls, clear all threads.
 void handle_SIGINT(int sig){
     interupted = 1;
+}
+
+
+void print_users(User** usres, int total_user){
+    for(int i = 0; i < total_user; i ++){
+        printf("User %d:\n", i);
+        printf("Username: %s.\n", usres[i]->username);
+        printf("Permission: %s.\n", usres[i]->permission_level);
+    }
+}
+
+void destroy_users(User** users, int total_user){
+    for(int i = 0; i < total_user; i ++){
+        free(users[i]->username);
+        free(users[i]->permission_level);
+        free(users[i]);
+    }
+    free(users);
 }
 
 
@@ -69,49 +219,73 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-    // Set interupt handler.
-    signal(SIGINT, handle_SIGINT);
-
     // Get the server time interval.
     double server_time_interval = strtod(argv[1], NULL);
     printf("Got the time interval: %f.\n", server_time_interval);
 
+
+    // Setup the valid users.
+    FILE* roles = fopen("../roles.txt", "r");
+    if(!roles){
+        perror("Failed to open role file.\n");
+        return 1;
+    }
+    // Initialize the users.
+    char username_buff[BUFF_LEN];
+    char permission_buff[BUFF_LEN];
+    while(fscanf(roles, "%s %s", username_buff, permission_buff) == 2){
+        printf("Valid username: %s.\n", username_buff);
+        printf("Permission level: %s.\n", permission_buff);
+        // Create a new user.
+        User* newUser = malloc(sizeof(User));
+        newUser->username = malloc(sizeof(char)*(strlen(username_buff)+1));
+        newUser->permission_level = malloc(sizeof(char)*(strlen(permission_buff)+1));
+        strcpy(newUser->username, username_buff);
+        strcpy(newUser->permission_level, permission_buff);
+
+        // Add the new user into users.
+        users = realloc(users, sizeof(User)*(total_user+1));
+        users[total_user] = newUser;
+        total_user ++;
+    }
+
+
+    // Set interupt handler.
+    signal(SIGINT, handle_SIGINT);
     // Register SIGRTMIN handler by sigaction.
     struct sigaction sa;
     sa.sa_flags = SA_SIGINFO;
-    sa.sa_handler = handle_SIGRTMIN;
+    sa.sa_sigaction = handle_SIGRTMIN;
     sigaction(SIGRTMIN, &sa, NULL);
-    // Clear the signal set.
-    sigemptyset(&sa.sa_mask);
-    // Mask the SIGRTMIN during the signal handler processing.
-    sigaddset(&sa.sa_mask, SIGRTMIN);
 
+    // // Clear the signal set.
+    // sigemptyset(&sa.sa_mask);
+    // // Mask the SIGRTMIN during the signal handler processing.
+    // sigaddset(&sa.sa_mask, SIGRTMIN);
 
-    // Creat the pipe to listen client connection.
-    if(mkfifo("connection", 0666) < 0){
-        perror("Failed to create pipe.\n");
-    }
-
-    int read_fd = open("connection", O_RDWR);
 
     // sigset_t mask;
     // sigemptyset(&mask);
     // sigaddset(&mask, SIGRTMIN);
+    char input_buff[BUFF_LEN];
     while (!interupted) {
-        pause();
+        scanf("%s", input_buff);
+        if(strcmp(input_buff, "QUIT") == 0){
+            handle_SIGINT(SIGINT);
+        }
     }
 
 
     // Join all threads.
+    printf("Not terminated threads: %d.\n", clinet_count);
     for(int i = 0; i < clinet_count; i++){
+        printf("Waiting for thread: %p.\n", (void*)threads[i]);
         pthread_join(threads[i], NULL);
     }
 
 
-    close(read_fd);
-    if (unlink("connection") < 0) {
-        perror("Failed to remove pipe");
-    }
+    // Free the users.
+    destroy_users(users, total_user);
 
     return 0;
 }
